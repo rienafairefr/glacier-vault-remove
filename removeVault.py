78ee6ca301a490a5a45c95e607efd9d0dbc80435
rienafairefr <rienafairefr@gmail.com>
2017-04-05 21:35:37 +0200

    no message

--- removeVault.py
+++ removeVault.py
@@ -9,83 +9,116 @@ import os
 import logging
 import boto3
 from multiprocessing import Process
-from socket import gethostbyname, gaierror
+
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+def humansize(nbytes):
+    if nbytes == 0: return '0 B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes) - 1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
 
 def split_list(alist, wanted_parts=1):
-	length = len(alist)
-	return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
-		for i in range(wanted_parts) ]
+    length = len(alist)
+    return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts]
+        for i in range(wanted_parts) ]
 
 def process_archive(archive_list):
-	logging.info('Starting work on %s items', len(archive_list))
-	for index, archive in enumerate(archive_list):
-		if archive['ArchiveId'] != '':
-			logging.info('%s Remove archive number %s of %s, ID : %s', os.getpid(), index, len(archive_list), archive['ArchiveId'])
-			try:
-				glacier.delete_archive(
-				    vaultName=vaultName,
-				    archiveId=archive['ArchiveId']
-				)
-			except:
-				printException()
+    worker_pid = os.getpid()
+    n_archives = len(archive_list)
+    logging.info('Starting work on %s items',n_archives)
+    for index, archive in enumerate(archive_list):
+        if archive['ArchiveId'] != '':
+            logging.info('%s Remove archive number %s of %s, ID : %s', worker_pid, index, n_archives, archive['ArchiveId'])
+            try:
+                glacier.delete_archive(
+                    vaultName=vaultName,
+                    archiveId=archive['ArchiveId']
+                )
+            except:
+                printException()
 
-				logging.info('Sleep 2s before retrying...')
-				time.sleep(2)
+                logging.info('Sleep 2s before retrying...')
+                time.sleep(2)
 
-				logging.info('Retry to remove archive ID : %s', archive['ArchiveId'])
-				try:
-					glacier.delete_archive(
-					    vaultName=vaultName,
-					    archiveId=archive['ArchiveId']
-					)
-					logging.info('Successfully removed archive ID : %s', archive['ArchiveId'])
-				except:
-					logging.error('Cannot remove archive ID : %s', archive['ArchiveId'])
+                logging.info('Retry to remove archive ID : %s', archive['ArchiveId'])
+                try:
+                    glacier.delete_archive(
+                        vaultName=vaultName,
+                        archiveId=archive['ArchiveId']
+                    )
+                    logging.info('Successfully removed archive ID : %s', archive['ArchiveId'])
+                except:
+                    logging.error('Cannot remove archive ID : %s', archive['ArchiveId'])
+    logging.info('%s Ended work on %s items',worker_pid,n_archives)
+
 
 def printException():
-	exc_type, exc_value = sys.exc_info()[:2]
-	logging.error('Exception "%s" occured with message "%s"', exc_type.__name__, exc_value)
+    exc_type, exc_value = sys.exc_info()[:2]
+    logging.error('Exception "%s" occured with message "%s"', exc_type.__name__, exc_value)
 
 # Default logging config
 logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO, datefmt='%H:%M:%S')
 
 # Get arguments
 if len(sys.argv) >= 3:
-	regionName = sys.argv[1]
-	vaultName = sys.argv[2]
+    regionName = sys.argv[1]
+    vaultName = sys.argv[2]
 else:
-	# If there are missing arguments, display usage example and exit
-	logging.error('Usage: %s <region_name> [<vault_name>|LIST] [DEBUG] [NUMPROCESS]', sys.argv[0])
-	sys.exit(1)
+    # If there are missing arguments, display usage example and exit
+    logging.error('Usage: %s <region_name> [<vault_name>|LIST] [DEBUG] [NUMPROCESS] [BLOCKSIZE]', sys.argv[0])
+    sys.exit(1)
+
 
 # Get custom logging level
-if len(sys.argv) == 4 and sys.argv[3] == 'DEBUG':
-	logging.info('Logging level set to DEBUG.')
-	logging.getLogger().setLevel(logging.DEBUG)
+if sys.argv[3] == 'DEBUG':
+    logging.info('Logging level set to DEBUG.')
+    logging.getLogger().setLevel(logging.DEBUG)
 
-# Get number of processes
 numProcess = 1
-if len(sys.argv) == 4:
-	if sys.argv[3].isdigit():
-		numProcess = int(sys.argv[3])
-elif len(sys.argv) == 5:
-	if sys.argv[4].isdigit():
-		numProcess = int(sys.argv[4])
+# Get number of processes
+if sys.argv[3] == 'DEBUG':
+    if len(sys.argv) >= 5:
+        if sys.argv[4].isdigit():
+            numProcess = int(sys.argv[4])
+else:
+    if len(sys.argv) >= 4:
+        if sys.argv[3].isdigit():
+            numProcess = int(sys.argv[3])
+
+bufferSize_MB = -1
+# Get bufferSize
+if sys.argv[3] == 'DEBUG':
+    if len(sys.argv) >= 6:
+        if sys.argv[5].isdigit():
+            bufferSize_MB = int(sys.argv[5])
+else:
+    if len(sys.argv) >= 5:
+        if sys.argv[4].isdigit():
+            bufferSize_MB = int(sys.argv[4])
+
 logging.info('Running with %s processes', numProcess)
+
+if bufferSize_MB==-1:
+    logging.info('Reading the inventory all at once')
+else:
+    logging.info('Streaming the inventory in blocks of %s MB', bufferSize_MB)
 
 os.environ['AWS_DEFAULT_REGION'] = regionName
 
 # Load credentials
 try:
-	f = open('credentials.json', 'r')
-	config = json.loads(f.read())
-	f.close()
+    f = open('credentials.json', 'r')
+    config = json.loads(f.read())
+    f.close()
 
- 	os.environ['AWS_ACCESS_KEY_ID'] = config['AWSAccessKeyId']
-	os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWSSecretKey']
+    os.environ['AWS_ACCESS_KEY_ID'] = config['AWSAccessKeyId']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWSSecretKey']
 
 except:
-	logging.error('Cannot load "credentials.json" file... Assuming Role Authentication.')
+    logging.error('Cannot load "credentials.json" file... Assuming Role Authentication.')
 
 sts_client = boto3.client("sts")
 accountId = sts_client.get_caller_identity()["Account"]
@@ -93,32 +126,32 @@ accountId = sts_client.get_caller_identity()["Account"]
 logging.info("Working on AccountID: {id}".format(id=accountId))
 
 try:
-	logging.info('Connecting to Amazon Glacier...')
-	glacier = boto3.client('glacier')
+    logging.info('Connecting to Amazon Glacier...')
+    glacier = boto3.client('glacier')
 except:
-	printException()
-	sys.exit(1)
+    printException()
+    sys.exit(1)
 
 if vaultName == 'LIST':
-	try:
-		logging.info('Getting list of vaults...')
-		response = glacier.list_vaults()
-	except:
-		printException()
-		sys.exit(1)
+    try:
+        logging.info('Getting list of vaults...')
+        response = glacier.list_vaults()
+    except:
+        printException()
+        sys.exit(1)
 
-	for vault in response['VaultList']:
-		logging.info(vault['VaultName'])
+    for vault in response['VaultList']:
+        logging.info(vault['VaultName'])
 
-	exit(0)
+    exit(0)
 
 try:
-	logging.info('Getting selected vault... [{v}]'.format(v=vaultName))
-	vault = glacier.describe_vault(vaultName=vaultName)
-	logging.info("Working on ARN {arn}".format(arn=vault['VaultARN']))
+    logging.info('Getting selected vault... [{v}]'.format(v=vaultName))
+    vault = glacier.describe_vault(vaultName=vaultName)
+    logging.info("Working on ARN {arn}".format(arn=vault['VaultARN']))
 except:
-	printException()
-	sys.exit(1)
+    printException()
+    sys.exit(1)
 
 logging.info('Getting jobs list...')
 response = glacier.list_jobs(vaultName=vaultName)
@@ -126,21 +159,21 @@ jobID = ''
 
 # Check if a job already exists
 for job in response['JobList']:
-	if job['Action'] == 'InventoryRetrieval':
-		logging.info('Found existing inventory retrieval job...')
-		jobID = job['JobId']
+    if job['Action'] == 'InventoryRetrieval':
+        logging.info('Found existing inventory retrieval job...')
+        jobID = job['JobId']
 
 if jobID == '':
-	logging.info('No existing job found, initiate inventory retrieval...')
-	try:
-		glacier_resource = boto3.resource('glacier')
-		vault = glacier_resource.Vault(accountId, vaultName)
-		job = vault.initiate_inventory_retrieval()
+    logging.info('No existing job found, initiate inventory retrieval...')
+    try:
+        glacier_resource = boto3.resource('glacier')
+        vault = glacier_resource.Vault(accountId, vaultName)
+        job = vault.initiate_inventory_retrieval()
 
-		jobID = job.id
-	except:
-		printException()
-		sys.exit(1)
+        jobID = job.id
+    except:
+        printException()
+        sys.exit(1)
 
 logging.info('Job ID : %s', jobID)
 
@@ -150,88 +183,111 @@ job = glacier.describe_job(vaultName=vaultName, jobId=jobID)
 logging.info('Job Creation Date: {d}'.format(d=job['CreationDate']))
 
 while job['StatusCode'] == 'InProgress':
-	# Job are usualy ready within 4hours of request.
-	logging.info('Inventory not ready, sleep for 10 mins...')
+    # Job are usualy ready within 4hours of request.
+    logging.info('Inventory not ready, sleep for 10 mins...')
 
-	time.sleep(60*10)
+    time.sleep(60*10)
 
-	job = glacier.describe_job(vaultName=vaultName, jobId=jobID)
+    job = glacier.describe_job(vaultName=vaultName, jobId=jobID)
 
-def treat_inventory(inventory):
-	archiveList = inventory['ArchiveList']
+def treat_inventory(archiveList):
+    logging.info('Removing %s archives... please be patient, this may take some time...', len(archiveList))
+    archiveParts = split_list(archiveList, numProcess)
+    jobs = []
 
-	logging.info('Removing %s archives... please be patient, this may take some time...', len(archiveList));
-	archiveParts = split_list(archiveList, numProcess)
-	jobs = []
+    for archive in archiveParts:
+        p = Process(target=process_archive, args=(archive,))
+        jobs.append(p)
+        p.start()
 
-	for archive in archiveParts:
-		p = Process(target=process_archive, args=(archive,))
-		jobs.append(p)
-		p.start()
+    for j in jobs:
+        j.join()
+    logging.info('all workers finished')
 
-	for j in jobs:
-		j.join()
+def remove_vault():
+    logging.info('Removing vault...')
+    try:
+        glacier.delete_vault(
+            vaultName=vaultName
+        )
+        logging.info('Vault removed.')
+    except:
+        printException()
+        logging.error('We cant remove the vault now. Please wait some time and try again. You can also remove it from the AWS console, now that all archives have been removed.')
 
+class Reader(object):
+    def __init__(self):
+        self.seek = 0
+
+    def read(self,n):
+        logging.info('range %u-%u',self.seek,self.seek+n)
+        returnvalue = glacier.get_job_output(vaultName=vaultName, jobId=job['JobId'],range='bytes=%d-%d'%(self.seek,self.seek+n))['body'].read(n).decode('utf-8')
+        self.seek += n
+        return returnvalue
 
 if __name__ == '__main__':
-	if job['StatusCode'] == 'Succeeded':
-		logging.info('Inventory retrieved, parsing data...')
-		job['jobId'] = 'Fcp_E3d54GiSehkmJH1M7NVXZVyQEM_RP_7B7Spvxo4m1CUknGNgk7CLlKzcP45yOMlYQAoYrVf0JLlTDCTEZzRwzg_q'
-		job_output = glacier.get_job_output(vaultName=vaultName, jobId=job['jobId'])
-		job_output_body = job_output['body']
+    if job['StatusCode'] == 'Succeeded':
+        logging.info('Inventory retrieved, parsing data...')
+        job_size = job['InventorySizeInBytes']
 
-		def read(n):
-			return job_output_body.read(n).decode('utf-8')
+        if bufferSize_MB==-1:
+            inventory = glacier.get_job_output(vaultName=vaultName, jobId=job['JobId'])['body'].read().decode('utf-8')
+            treat_inventory(inventory)
+            remove_vault()
+            exit(0)
 
-		prefix = ''
-		while True:
-			prefix += read(1)
-			try:
-				schema = json.loads(prefix + ']}')
-				# Okay, we have our prefix
-				break
-			except:
-				pass
-		bufferSize = 1024 * 1024
-		if 'ArchiveList' not in schema:
-			logging.error('Couldnt retrieve inventory, expecting ArchiveList key in job output')
-		suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+        bufferSize = bufferSize_MB * 1024 * 1024
+        reader = Reader()
 
-		def humansize(nbytes):
-			if nbytes == 0: return '0 B'
-			i = 0
-			while nbytes >= 1024 and i < len(suffixes) - 1:
-				nbytes /= 1024.
-				i += 1
-			f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
-			return '%s %s' % (f, suffixes[i])
-		logging.info('Streaming a %s archives list in blocks of roughly %s '%(humansize(job['InventorySizeInBytes']),humansize(bufferSize)))
-		archiveList = ''
-		while True:
-			# read a big block
-			archiveList = read(bufferSize)
-			while True:
-				# read a character until the string is json-parsable
-				try:
-					inventory = json.loads(prefix + archiveList + ']}')
-					# json parsed okay, we have our json string
-					break
-				except:
-					archiveList += read(1)
-			treat_inventory(inventory)
-			# consume the comma before the next items
-			next = read(1)
-			if next == ']' or next=='':
-				# reached the end !
-				logging.info('OK, removed all the archives')
-				break
+        logging.info(
+            'Streaming a %s archives list in blocks of roughly %s ' % (humansize(job_size), humansize(bufferSize)))
 
-	logging.info('Removing vault...')
-	try:
-		glacier.delete_vault(
-		    vaultName=vaultName
-		)
-		logging.info('Vault removed.')
-	except:
-		printException()
-		logging.error('We cant remove the vault now. Please wait some time and try again. You can also remove it from the AWS console, now that all archives have been removed.')
+        prefix = reader.read(bufferSize)
+        for i in range(1,bufferSize):
+            try:
+                schema = json.loads(prefix[0:i] + ']}')
+                # Okay, we have our prefix
+                archiveList=prefix[i:]
+                prefix = prefix[0:i]
+                break
+            except:
+                pass
+        if 'ArchiveList' not in schema:
+            logging.error('Couldnt retrieve inventory, expecting ArchiveList key in job output')
+
+        open('archiveList', 'w').write(archiveList)
+        pass
+        average_object_size = 460
+        is_last = False
+
+
+        def get_data():
+
+        while True:
+            testparse = prefix + archiveList
+            try:
+                inventory = json.loads(testparse)
+                #last case
+                is_last=True
+            except:
+                for i in range(1,5000):
+                    toparse = prefix + archiveList[:-i] + ']}'
+                    #open('toparse.json','w').write(toparse)
+                    logging.debug('Trying to parse json length %u %s [...] %s ', len(toparse), toparse[:100], toparse[-100:])
+                    try:
+                        inventory = json.loads(toparse)
+                        # OK found a json parsable
+                        archiveList = archiveList[-i+1:]
+                        archiveList += reader.read(bufferSize)
+                        break
+                    except:
+                        pass
+
+
+            #treat_inventory(inventory['ArchiveList'])
+            logging.info('treating an inventory of %u items', len(inventory['ArchiveList']))
+            # treat_inventory(inventory['ArchiveList'])
+            # read a big block
+            if is_last:
+                break
+
